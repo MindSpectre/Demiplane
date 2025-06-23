ac67dd48570738342975b3f13b16bcd38e4885a1
Polaris <neuronspectrelin@gmail.com>
2025-06-23 03:31:23 +0300

    Align logging macros and includes with formatting guidelines
    - Unified spacing in `log_macros.hpp` and `log_macros_adds.hpp` for improved readability.
    - Adjusted include order in `logger.cpp` to prevent reordering issues.

--- common/scroll/logger/include/log_macros.hpp
+++ common/scroll/logger/include/log_macros.hpp
@@ -5,19 +5,19 @@
 #define SCROLL_LOG_DIRECT_WRN(logger_ptr, message) SCROLL_LOG_ENTRY(logger_ptr, demiplane::scroll::WRN, message)
 #define SCROLL_LOG_DIRECT_ERR(logger_ptr, message) SCROLL_LOG_ENTRY(logger_ptr, demiplane::scroll::ERR, message)
 #define SCROLL_LOG_DIRECT_FAT(logger_ptr, message) SCROLL_LOG_ENTRY(logger_ptr, demiplane::scroll::FAT, message)
-#define SCROLL_LOG_DIRECT_STREAM_DBG(logger_ptr) SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::DBG)
-#define SCROLL_LOG_DIRECT_STREAM_INF(logger_ptr) SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::INF)
-#define SCROLL_LOG_DIRECT_STREAM_WRN(logger_ptr) SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::WRN)
-#define SCROLL_LOG_DIRECT_STREAM_ERR(logger_ptr) SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::ERR)
-#define SCROLL_LOG_DIRECT_STREAM_FAT(logger_ptr) SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::FAT)
+#define SCROLL_LOG_DIRECT_STREAM_DBG(logger_ptr)   SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::DBG)
+#define SCROLL_LOG_DIRECT_STREAM_INF(logger_ptr)   SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::INF)
+#define SCROLL_LOG_DIRECT_STREAM_WRN(logger_ptr)   SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::WRN)
+#define SCROLL_LOG_DIRECT_STREAM_ERR(logger_ptr)   SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::ERR)
+#define SCROLL_LOG_DIRECT_STREAM_FAT(logger_ptr)   SCROLL_LOG_STREAM_ENTRY(logger_ptr, demiplane::scroll::FAT)
 
 
 #ifdef ENABLE_LOGGING
 #define SCROLL_LOG_ENTRY(logger_ptr, level, message) logger_ptr->log(level, message, std::source_location::current())
-#define SCROLL_LOG_STREAM_ENTRY(logger_ptr, level) demiplane::scroll::StreamLogEntry(logger_ptr, level)
+#define SCROLL_LOG_STREAM_ENTRY(logger_ptr, level)   demiplane::scroll::StreamLogEntry(logger_ptr, level)
 #else
 #define LOG_ENTRY(logger_ptr, level, message) (void(0))
-#define LOG_STREAM_ENTRY(logger_ptr, level) demiplane::scroll::DummyStreamLogEntry()
+#define LOG_STREAM_ENTRY(logger_ptr, level)   demiplane::scroll::DummyStreamLogEntry()
 
 namespace demiplane::scroll {
     class DummyStreamLogEntry {
@@ -27,5 +27,5 @@ namespace demiplane::scroll {
             return *this;
         }
     };
-}
+} // namespace demiplane::scroll
 #endif
--- common/scroll/logger_provider/include/log_macros_adds.hpp
+++ common/scroll/logger_provider/include/log_macros_adds.hpp
@@ -4,17 +4,16 @@
 // There are main macros
 
 #ifndef DMP_LOG_LEVEL
-#  define DMP_LOG_LEVEL DBG                // 0=OFF 1=ERR 2=WRN 3=INF 4=DBG 5=TRC
+#define DMP_LOG_LEVEL DBG // 0=OFF 1=ERR 2=WRN 3=INF 4=DBG 5=TRC
 #endif
 
-#define SCROLL_LOG()                 CAT(SCROLL_LOG_,             DMP_LOG_LEVEL)()
-#define SCROLL_LOG_MESSAGE(msg)      CAT(SCROLL_LOG_MESSAGE_,     DMP_LOG_LEVEL)(msg)
-#define SCROLL_LOG_DBG() SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::DBG)
-#define SCROLL_LOG_INF() SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::INF)
-#define SCROLL_LOG_WRN() SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::WRN)
-#define SCROLL_LOG_ERR() SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::ERR)
-#define SCROLL_LOG_FAT() SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::FAT)
-
+#define SCROLL_LOG()            CAT(SCROLL_LOG_, DMP_LOG_LEVEL)()
+#define SCROLL_LOG_MESSAGE(msg) CAT(SCROLL_LOG_MESSAGE_, DMP_LOG_LEVEL)(msg)
+#define SCROLL_LOG_DBG()        SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::DBG)
+#define SCROLL_LOG_INF()        SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::INF)
+#define SCROLL_LOG_WRN()        SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::WRN)
+#define SCROLL_LOG_ERR()        SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::ERR)
+#define SCROLL_LOG_FAT()        SCROLL_LOG_STREAM_ENTRY(this->get_logger(), demiplane::scroll::FAT)
 
 
 #define SCROLL_LOG_MESSAGE_DBG(message) SCROLL_LOG_ENTRY(this->get_logger(), demiplane::scroll::DBG, message)
@@ -22,9 +21,6 @@
 #define SCROLL_LOG_MESSAGE_WRN(message) SCROLL_LOG_ENTRY(this->get_logger(), demiplane::scroll::WRN, message)
 #define SCROLL_LOG_MESSAGE_ERR(message) SCROLL_LOG_ENTRY(this->get_logger(), demiplane::scroll::ERR, message)
 #define SCROLL_LOG_MESSAGE_FAT(message) SCROLL_LOG_ENTRY(this->get_logger(), demiplane::scroll::FAT, message)
-
-
-
 
 
 // Addons

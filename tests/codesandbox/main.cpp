70268a7b46af0438026675c359e4a44ee5a249c1
Polaris <neuronspectrelin@gmail.com>
2025-07-19 21:27:18 +0300

    Refactored `Timer` with new `CancellationToken`, updated APIs, and improved threading logic

--- tests/codesandbox/main.cpp
+++ tests/codesandbox/main.cpp
@@ -1,7 +1,7 @@
 #include <chrono>
+#include <demiplane/chrono>
 #include <iostream>
 #include <thread>
-#include <demiplane/chrono>
 
 using namespace std::chrono_literals;
 
@@ -18,34 +18,38 @@ int find_prims(int a) {
     return a;
 }
 
-void heavy_work(const demiplane::chrono::cancellation_token& token) {
+void heavy_work(const demiplane::chrono::CancellationToken& token) {
     for (int i = 0; !token.stop_requested(); ++i) {
-        demiplane::chrono::sleep_for(100ms);
+        demiplane::chrono::sleep_for(10ms);
         std::cout << "heavy work " << i << std::endl;
     }
 }
 
 int main() {
-    auto token = demiplane::chrono::cancellation_token{};
+    auto token = demiplane::chrono::CancellationToken{};
     std::cout << "main: " << std::this_thread::get_id() << std::endl;
-    demiplane::chrono::Timer t{5000ms};
-    std::jthread th{
-        [&token]() {
-            demiplane::chrono::sleep_for(500ms);
-            token.cancel();
-            std::cout << "cancel from thread " << std::this_thread::get_id() << std::endl;
-        }
-    };
+    demiplane::chrono::Timer t;
+    std::jthread             th{[&token]() {
+        demiplane::chrono::sleep_for(100ms);
+        token.cancel();
+        std::cout << "cancel from thread " << std::this_thread::get_id() << std::endl;
+    }};
     // 1. Polite vanish: callable accepts token
-    std::future<void> future_ok = t.execute_polite_vanish(token, &heavy_work);
+    std::future<void> future_ok = t.execute_polite_vanish(50ms, &heavy_work, token);
     //
 
 
     future_ok.wait();
+    token.renew();
+    std::jthread th2{[&token]() {
+        demiplane::chrono::sleep_for(900ms);
+        token.cancel();
+        std::cout << "cancel from thread " << std::this_thread::get_id() << std::endl;
+    }};
     // 2. Violent kill: legacy callable
-    auto future_legacy = t.execute_violent_kill(token, &find_prims, 123456789);
+    auto future_legacy = t.execute_violent_kill(3000ms, token, &find_prims, 123456789);
     // cancel from outside
-
+    future_legacy.wait();
 
     return 0;
 }

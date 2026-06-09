6ad05bc5b9c99a4caa6e31e3594f677d2e9fe53a
Polaris <neuronspectrelin@gmail.com>
2026-06-09 23:41:03 +0300

    test(http/types): cover all HttpMethod/HttpVersion enumerators in http_enums tests

--- tests/unit_tests/http/types/test_http_enums.cpp
+++ tests/unit_tests/http/types/test_http_enums.cpp
@@ -6,13 +6,22 @@ using namespace demiplane::http;
 TEST(HttpEnumsTest, MethodToString) {
     EXPECT_EQ(to_string(HttpMethod::get),     std::string_view{"GET"});
     EXPECT_EQ(to_string(HttpMethod::post),    std::string_view{"POST"});
+    EXPECT_EQ(to_string(HttpMethod::put),     std::string_view{"PUT"});
+    EXPECT_EQ(to_string(HttpMethod::patch),   std::string_view{"PATCH"});
     EXPECT_EQ(to_string(HttpMethod::del),     std::string_view{"DELETE"});
+    EXPECT_EQ(to_string(HttpMethod::head),    std::string_view{"HEAD"});
     EXPECT_EQ(to_string(HttpMethod::options), std::string_view{"OPTIONS"});
+    EXPECT_EQ(to_string(HttpMethod::unknown), std::string_view{"UNKNOWN"});
 }
 
 TEST(HttpEnumsTest, MethodFromBeast) {
     EXPECT_EQ(method_from_beast(boost::beast::http::verb::get),     HttpMethod::get);
+    EXPECT_EQ(method_from_beast(boost::beast::http::verb::post),    HttpMethod::post);
+    EXPECT_EQ(method_from_beast(boost::beast::http::verb::put),     HttpMethod::put);
+    EXPECT_EQ(method_from_beast(boost::beast::http::verb::patch),   HttpMethod::patch);
     EXPECT_EQ(method_from_beast(boost::beast::http::verb::delete_), HttpMethod::del);
+    EXPECT_EQ(method_from_beast(boost::beast::http::verb::head),    HttpMethod::head);
+    EXPECT_EQ(method_from_beast(boost::beast::http::verb::options), HttpMethod::options);
     EXPECT_EQ(method_from_beast(boost::beast::http::verb::unknown), HttpMethod::unknown);
 }
 
@@ -26,6 +35,8 @@ TEST(HttpEnumsTest, StatusCodeNumericValue) {
 }
 
 TEST(HttpEnumsTest, VersionNumeric) {
+    EXPECT_EQ(static_cast<unsigned>(HttpVersion::http_1_0), 10u);
     EXPECT_EQ(static_cast<unsigned>(HttpVersion::http_1_1), 11u);
     EXPECT_EQ(static_cast<unsigned>(HttpVersion::http_2),   20u);
+    EXPECT_EQ(static_cast<unsigned>(HttpVersion::http_3),   30u);
 }

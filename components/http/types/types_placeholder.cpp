bf08ed0f0489f28fdc344824c43cae18f18af9cc
Polaris <neuronspectrelin@gmail.com>
2026-06-10 01:28:25 +0300

    chore(http/types): drop bootstrap placeholder TU
    Every Types subdirectory now has real source.

--- components/http/types/types_placeholder.cpp
+++
@@ -1,5 +0,0 @@
-// Placeholder TU to give Demiplane::Component::Http::Types a non-empty
-// compilation unit during PR 1 bootstrap. Deleted in the final task.
-namespace demiplane::http::detail {
-    inline constexpr int types_layer_present = 1;
-}
